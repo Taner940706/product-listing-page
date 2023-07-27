c0fdb61f8d1340839d9f0b78fa6f66fa4ecc6329
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-07-27 15:30:30 +0300

    create main components

---
+++ product/src/App.js
@@ -0,0 +1,13 @@
+import NavBar from "./Components/Navigation/NavBar";
+import Products from "./Components/Products/Products";
+
+
+function App() {
+  return <>
+      <NavBar />
+      <Products />
+  </>
+
+}
+
+export default App;

98967fa5cd641c8fd72bb332f9eaa283c19d2081
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-07-28 13:56:40 +0300

    price component

--- product/src/Components/Filters/Price/Price.js
+++ product/src/Components/Filters/Price/Price.js
@@ -3,8 +3,9 @@ import "./Price.css"
 
 export default function Price() {
   return (
-    <div>
-      
-    </div>
+    <div className='slide-container'>
+      <input type="range" min="1" max="100" className='slider' id="myRange" />
+      <p>Value: <span id="price-value">88$</span></p>
+</div>
   )
 }

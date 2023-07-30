948dbfa801b01a7c5c226d8b645ccaaaa26489eb
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-07-30 19:52:10 +0300

    Update Inputs.js

--- product/src/Components/Inputs.js
+++ product/src/Components/Inputs.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function Inputs() {
+export default function Inputs({handleChangeEvent, value, title, name, color}) {
   return (
     <div>
        <label className='color-label-title'>
-        <input type="radio" />
-        <span></span>All
+        <input onChange={handleChangeEvent} type="radio" value={value} name={name} />
+        <span style={{backgroundColor: color}}></span>{title}
       </label>
     </div>
   )

209f1c42217d5c74b7e09a65b01395e9cd0eca95
Taner Ismail <59261346+Taner940706@users.noreply.github.com>
2023-07-30 20:02:08 +0300

    Update Color.js

--- product/src/Components/Filters/Color/Color.js
+++ product/src/Components/Filters/Color/Color.js
@@ -2,10 +2,15 @@ import React from 'react'
 import Inputs from '../../Inputs'
 import './Color.css'
 
-export default function Color() {
+export default function Color(handleChangeEvent) {
   return (
     <div className='sidebar-color-picker'>
-     <Inputs />
+     <Inputs
+     handleChangeEvent={handleChangeEvent}
+     value="white"
+     title="Black"
+     name="name1"
+     color="black" />
     </div>
   )
 }
